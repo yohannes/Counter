a099ee1bd8ee1124af8058eab37d3ffbf78588d7
Yohannes Wijaya <readyforservice@icloud.com>
2016-09-07 14:29:38 +0700

    - Implemented the logic of incrementing / decrementing numbers upon long press gesture on + / - button in 2 ways; programmatically for increment and via Interface Builder for decrement. - Refactored the methods in Counter View Controller

--- Counter/CounterViewController.swift
+++ Counter/CounterViewController.swift
@@ -6,15 +6,19 @@
 //  Copyright © 2016 Yohannes Wijaya. All rights reserved.
 //
 
-
-// TODO: 1) change icon background to transparent. 2) when user tap and hold on either + / -, counter will keep incrementing/decrementing until touch up. 3) set max to 999 and min to -999
 import UIKit
 
 class CounterViewController: UIViewController {
     
     // MARK: - Stored Properties
     
-    var counter: Int!
+    var counter = 0
+    let firingInterval = 0.1
+    
+    var timer: Timer?
+    
+    var plusButtonTitle: String!
+    var minusButtonTitle: String!
     
     // MARK: - IBOutlet Properties
     
@@ -26,13 +30,11 @@ class CounterViewController: UIViewController {
     // MARK: - IBAction Methods
     
     @IBAction func plusButtonDidTouch(_ sender: UIButton) {
-        guard let validButtonTitle = self.plusButton.currentTitle else { return }
-        self.processCounterWith(operator: validButtonTitle)
+        self.processCounterWith(operator: self.plusButtonTitle)
     }
     
     @IBAction func minusButtonDidTouch(_ sender: UIButton) {
-        guard let validButtonTitle = self.minusButton.currentTitle else { return }
-        self.processCounterWith(operator: validButtonTitle)
+        self.processCounterWith(operator: self.minusButtonTitle)
     }
     
     @IBAction func resetButtonDidTouch(_ sender: UILabel) {
@@ -43,20 +45,66 @@ class CounterViewController: UIViewController {
         self.minusButton.isEnabled = true
     }
     
+    @IBAction func minusButtonDidLongPress(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.began {
+            self.timer = Timer.scheduledTimer(timeInterval: self.firingInterval, target: self, selector: #selector(CounterViewController.decrementCounter), userInfo: nil, repeats: true)
+        }
+        else if sender.state == .ended || sender.state == .cancelled {
+            self.timer?.invalidate()
+            self.timer = nil
+        }
+    }
+    
     // MARK: - UIViewController Methods
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.counter = 0
+        
+        guard let validPlusbuttonTitle = self.plusButton.currentTitle else { return }
+        self.plusButtonTitle = validPlusbuttonTitle
+        
+        guard let validMinusButtonTitle = self.minusButton.currentTitle else { return }
+        self.minusButtonTitle = validMinusButtonTitle
+        
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(CounterViewController.plusButtonDidLongPress(sender:)))
+        longPressGestureRecognizer.minimumPressDuration = 0.25
+        self.plusButton.addGestureRecognizer(longPressGestureRecognizer)
     }
     
     // MARK: - Helper Methods
     
-    fileprivate func processCounterWith(operator operation: String) {
+    @objc fileprivate func incrementCounter() {
+        self.counter = self.counter + 1
+        self.numberLabel.text = String(self.counter)
+        
+        self.toggleButtonsState()
+    }
+    
+    @objc fileprivate func decrementCounter() {
+        self.counter  = self.counter - 1
+        self.numberLabel.text = String(self.counter)
+        
+        self.toggleButtonsState()
+    }
+    
+    @objc fileprivate func processCounterWith(operator operation: String) {
         self.counter = operation == "+" ? self.counter + 1 : self.counter - 1
         self.numberLabel.text = String(self.counter)
         
+        self.toggleButtonsState()
+    }
+    
+    @objc fileprivate func plusButtonDidLongPress(sender: UILongPressGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.began {
+            self.timer = Timer.scheduledTimer(timeInterval: self.firingInterval, target: self, selector: #selector(CounterViewController.incrementCounter), userInfo: nil, repeats: true)
+        }
+        else if sender.state == .ended || sender.state == .cancelled {
+            self.timer?.invalidate()
+            self.timer = nil
+        }
+    }
+    
+    fileprivate func toggleButtonsState() {
         if self.counter >= 999 {
             self.plusButton.isEnabled = false
             self.minusButton.isEnabled = true
